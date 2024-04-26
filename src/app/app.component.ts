b34c5f5f5789d6ce8aa6b277f8f1dedc0343e9e4
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-04-26 18:57:51 +0330

    #

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  username : string = ""
-  public onUsernameInputChanged(e: Event){
-    this.username = (<HTMLInputElement> e.target).value
-   
+  username: string = '';
+  password: string = '';
+  retypePassword: string = '';
+  isPasswordsMatch: boolean = false;
+  public onUsernameInputChanged(e: Event) {
+    this.username = (<HTMLInputElement>e.target).value;
   }
-  
+  public onPasswordInputChanged(e: Event) {
+    this.password = (<HTMLInputElement>e.target).value;
+  }
+
+  public onRetypePasswordInputChanged(e: Event) {
+    this.retypePassword = (<HTMLInputElement>e.target).value
+    this.isPasswordsMatch =
+      this.password === this.retypePassword ? true : false;
+  }
 }
